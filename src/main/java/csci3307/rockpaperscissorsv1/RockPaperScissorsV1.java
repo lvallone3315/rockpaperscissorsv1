43f1da2ed3e4b75d9baec2dc4277adfa3910ac01
lvallone3315 <55007696+lvallone3315@users.noreply.github.com>
2022-02-01 14:08:55 -0500

    Rock, Paper, Scissors (RPS) - Print Intro Message

--- src/main/java/csci3307/rockpaperscissorsv1/RockPaperScissorsV1.java
+++ src/main/java/csci3307/rockpaperscissorsv1/RockPaperScissorsV1.java
@@ -7,15 +7,22 @@ package csci3307.rockpaperscissorsv1;
 
 /**
  *
- * @author User
+ * @author Lee
+ * 
+ * Play a simple game of rock, paper scissors
  */
 public class RockPaperScissorsV1 {
-
+    static final String RPS_INTRO_MESSAGE = "\tWelcome to Rock, Paper, Scissors\n"
+            + "Each round, player inputs 0 (Rock), 1 (Paper) or 2 (Scissors),\n"
+            + "Computer will randomly select it's choice\n"
+            + "and the round result will be displayed.\n"
+            + "The game continues until you close the program\n";
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println(RPS_INTRO_MESSAGE);
+        
     }
     
 }
