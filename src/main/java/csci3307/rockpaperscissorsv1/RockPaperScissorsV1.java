2880f2be065910d0ff3942938441fa221b52ceef
lvallone3315 <55007696+lvallone3315@users.noreply.github.com>
2022-02-01 19:33:36 -0500

    RPS - Refactored using helper functions making code more readable & easier to change.   Removed references to Rock, Paper & Scissors from main.   Heading towards creating objects.   Note: Still hard coding # of choices in main

--- src/main/java/csci3307/rockpaperscissorsv1/RockPaperScissorsV1.java
+++ src/main/java/csci3307/rockpaperscissorsv1/RockPaperScissorsV1.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package csci3307.rockpaperscissorsv1;
 
 import java.util.Random;
@@ -13,6 +8,12 @@ import java.util.Scanner;
  * @author Lee
  * 
  * Play a simple game of rock, paper scissors
+ * 
+ * Refactored version using helper functions making code more readable &
+ * easier to change.
+ * 
+ * Helper functions and Intro message know the choices - ie main loop doesn't care
+ * Still hardcoding # of choices in main
  */
 public class RockPaperScissorsV1 {
     static final String RPS_INTRO_MESSAGE = "\tWelcome to Rock, Paper, Scissors\n"
@@ -31,76 +32,83 @@ public class RockPaperScissorsV1 {
                 // Initialize Scanner for Getting Input from User
         Scanner playerInput = new Scanner(System.in);
  
-                // Print prompt - get player choice from console
-        System.out.println(PLAYER_PROMPT);
-        int playerChoice = playerInput.nextInt();
+  
+        int playerChoice;
         
             // Generate random number in range 0..2
         Random r = new Random();
-        int computerChoice = r.nextInt(3);
+        int computerChoice;
                 
         
         /*
-         * Approach - 3 player choices - 3 computer choices - so 9 cases
-         *   Exhaustive implemententation
-         *      switch on player choice, for each choice, switch on comp choice
-         * ToDo - this doesn't scale well & is hard to read even with 3 choices
+         * Refactored using helper functions
         */
         while (true) {
-            switch (playerChoice) {
-                case 0:  // player chose Rock
-                    System.out.println("You picked ROCK");
-                    switch (computerChoice) {
-                        case 0:
-                            System.out.println("You both picked Rock.  It's a tie!");
-                            break;
-                        case 1:
-                            System.out.println("Paper covers rock.  Computer wins!");
-                            break;
-                        case 2:
-                            System.out.println("Rock dulls scissors.  You win!");
-                            break;
-                        default:  // null default - included for readability, should never get here
-                    }
-                    break;  // end Rock
-                case 1:     // player chose Paper
-                    System.out.println("You picked PAPER");
-                    switch (computerChoice) {
-                        case 0:
-                            System.out.println("Paper covers rock.  You win!");
-                            break;
-                        case 1:
-                            System.out.println("You both picked paper.  It's a tie!");
-                            break;
-                        case 2:
-                            System.out.println("Scissors cut paper.  Computer wins!");
-                            break;
-                        default:  // null default - included for readability, should never get here
-                    }
-                    break;  // end Paper
-                case 2:     // player chose Scissors
-                    System.out.println("You picked SCISSORS");
-                    switch (computerChoice) {
-                        case 0:
-                            System.out.println("Rock dulls scissors.  Computer wins!");
-                            break;
-                        case 1:
-                            System.out.println("Scissors cut paper.  You win!");
-                            break;
-                        case 2:
-                            System.out.println("You both picked scissors.  It's a tie!");
-                            break;
-                        default:  // null default - included for readability, should never get here
-                    }
-                    break; // end Scissors
-                default:
-                    System.out.println("INVALID CHOICE:" + PLAYER_PROMPT);
-            }
-            
-            // get input for next round
-            // ToDo - add a case for quitting based on user input
+                // Get player choice from console & validate
+            System.out.println(PLAYER_PROMPT);
             playerChoice = playerInput.nextInt();
+            if ((playerChoice < 0) || (playerChoice > 2)) {
+                System.out.println("Invalid Input - please select 0, 1 or 2");
+                continue;
+            }
+            System.out.println("You picked " + helperFunctions.getRPS_String(playerChoice));
+            
+                // Get Computer Choice - nextInt => 0 <= # < 3
             computerChoice = r.nextInt(3);
+            System.out.println("Computer picked " + helperFunctions.getRPS_String(computerChoice));
+            
+                // Score round
+            if (helperFunctions.playerWon(playerChoice, computerChoice))
+                    System.out.println("Player Won!");
+            else if (helperFunctions.playerTied (playerChoice, computerChoice))
+                System.out.println("It's a Tie!");
+            else
+                System.out.println("Computer Won!");
+        }  // end game loop
+    }  // end main
+}
+
+class helperFunctions {    
+    public static boolean playerWon (int playerMove, int computerMove) {
+        switch (playerMove) {
+            case 0:  // if player chose Rock
+                if (computerMove == 2)  // and computer chose scissors
+                    return true;
+                else
+                    return false;
+            case 1:     // if player chose Paper
+                if (computerMove == 0)  // and computer chose rock
+                    return true;
+                else
+                    return false;
+            case 2:     // if player chose Scissors
+                if (computerMove == 1)  // and computer chose paper
+                    return true;
+                else
+                    return false;
+            default:
+                // included for readability, should never get here
+                return false;
+        }
+    }
+    
+    public static boolean playerTied(int playerMove, int computerMove) {
+        if (playerMove == computerMove)
+            return true;
+        else
+            return false;
+    }
+    
+    public static String getRPS_String(int move) {
+        switch (move) {
+            case 0:
+                return ("Rock");
+            case 1:
+                return ("Paper");
+            case 2:
+                return ("Scissors");
+            default:
+                return ("Invalid Input");
         }
     }
 }
